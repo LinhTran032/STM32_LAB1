f024425d7a7ad49883fb80e770a46383b633f636
linh-tranha_03 <linh.tranhahcmut@hcmut.edu.vn>
2024-10-08 17:17:40 +0700

    Ex1

--- LAB/Core/Src/main.c
+++ LAB/Core/Src/main.c
@@ -92,6 +92,12 @@ int main(void)
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1){
+	  	HAL_GPIO_WritePin ( LED_RED_GPIO_Port , LED_RED_Pin , SET );
+	  	HAL_GPIO_WritePin ( LED_YELLOW_GPIO_Port , LED_YELLOW_Pin , RESET );
+	  	HAL_Delay (2000) ;
+	  	HAL_GPIO_WritePin ( LED_RED_GPIO_Port , LED_RED_Pin , RESET );
+	  	HAL_GPIO_WritePin ( LED_YELLOW_GPIO_Port , LED_YELLOW_Pin , SET );
+	  	HAL_Delay (2000) ;
     }
 }
 /**
